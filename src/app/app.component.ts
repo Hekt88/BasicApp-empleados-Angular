76e27e9c0a12d0779ddbd19ff93f1a700505a505
Hekt88 <hectorllorente98@gmail.com>
2024-03-01 12:30:24 +0100

    app-empleados angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[]=[
+
+    new Empleado("Héctor", "Apellido", "Presidente", 7500),
+    new Empleado("Walter", "White", "Director", 5500),
+    new Empleado("Tony", "Montana", "Jefe de seccion", 4500),
+    new Empleado("Luka", "Modric", "Administrativo", 3500),
+
+
+  ];
+
+
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    this.empleados.push(miEmpleado);
+  }
+
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
 }
